0514b98363cf7f9ca0c335aef53156484de098cc
lUCIANOGFORTES02 <lucianogfortes@gmail.com>
2025-08-07 11:55:55 -0300

    feat: marcas parceiras

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,6 +24,26 @@ export default async function Home() {
 
   const categories = await db.query.categoryTable.findMany({});
 
+  const marcas = [
+    {
+      image: "/simple-icons_adidas.svg",
+      title: "Adidas",
+    },
+
+    {
+      image: "/simple-icons_nike.svg",
+      title: "Nike",
+    },
+    {
+      image: "/simple-icons_puma.svg",
+      title: "Puma",
+    },
+    {
+      image: "/simple-icons_newbalance.svg",
+      title: "New Balance",
+    },
+  ];
+
   return (
     <>
       <Header />
@@ -37,6 +57,31 @@ export default async function Home() {
             sizes="100vw"
             className="h-auto w-full"
           />
+        </div>
+
+        <div className="space-y-6 px-5">
+          <h3 className="font-semibold">Marcas parceiras</h3>
+          <div className="flex gap-6 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+            {marcas.map((marca) => (
+              <div
+                key={marca.title}
+                className="flex flex-col items-center gap-4"
+              >
+                <div className="b-[#F1F1F1] flex h-20 w-20 items-center justify-center gap-6 rounded-2xl border-[1.6px]">
+                  <Image
+                    src={marca.image}
+                    alt={marca.title}
+                    width={32}
+                    height={32}
+                    className="h-8 w-8 object-contain"
+                  />
+                </div>
+                <p className="flex truncate text-sm font-medium">
+                  {marca.title}
+                </p>
+              </div>
+            ))}
+          </div>
         </div>
 
         <ProductList products={products} title="Mais vendidos" />
